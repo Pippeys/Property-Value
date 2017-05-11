b87945048c44b4a8a0280cdfe8c6ea784581832e
Scott Standring <scott@standring.com>
2017-05-10 17:03:54 -0700

    Basic improvements, Moving forward

--- Property_Estimator.py
+++ Property_Estimator.py
@@ -20,42 +20,50 @@ import pickle
 
 # Loading Data
 # Mac
-#df = pd.read_csv('/Users/magicsoccer10/Dropbox/twerk werk/Property Value/07-17.csv')
+#df = pd.read_csv('C:/Users/magicsoccer10/Dropbox/twerk werk/cre_values')
 
 # Arch
-#df = pd.read_csv('C:/Users/scott/Dropbox/twerk werk/Property Value/07-17.csv')
+#df = pd.read_csv('C:/Users/scott/Dropbox/twerk werk/cre_values')
 
 # Work
-df = pd.read_csv('C:/Users/sstandring/Dropbox/twerk werk/Property Value/07-17.csv')
+
+
 
 # Transforming data for modeling (based on exploration: (See Jupyter/Learning_Satyrn))
-df['sale_date'] = pd.to_datetime(df['Sale Date'])
-df['sale_quarter'] = df['sale_date'].dt.quarter
-df['sale_quarter'] = df['sale_quarter'].astype('int')
-df['log_price'] = np.log(df.Sale_Price)
-df['log_sqft'] = np.log(df['Bldg SF'])
-df['log_far'] = np.log(df['Floor Area Ratio'])
-num = LabelEncoder()
-df['bin_zip'] = num.fit_transform(df['Property Zip Code'].astype('str'))
+def transform(df):
+    df['log_price'] = np.log(df.Sale_Price)
+    df['log_sqft'] = np.log(df['Bldg SF'])
+    num = LabelEncoder()
+    df['Property Zip Code'] = df['Property Zip Code'].astype(str).str[:-2]
+    df['bin_zip'] = num.fit_transform(df['Property Zip Code'].astype('str'))
+    # Filtering by Multi-Family and Create modelling DataFrame
+    mfdf = df.loc[df['PropertyType'] == 'Multi-Family']
+    mfquant = mfdf[['log_price','Cap_Rate','log_sqft','bin_zip']]
+    return mfquant
 
-# Filtering by Multi-Family and Create modelling DataFrame
-mfdf = df.loc[df['PropertyType'] == 'Multi-Family']
-mfquant = mfdf[['log_price','Cap_Rate','log_sqft','bin_zip']]
+def regression (x,y):
+    #Regression Model
+    LinReg = LinearRegression(normalize=True)
+    LinReg.fit(x,y)
+    #RFE(LinReg,3)
+    #estimator = SVR(kernel='linear')
+    #selector = RFE(estimator, 5, step=1)
+    #selector.support_
+    #selector.ranking_
+    joblib.dump(LinReg, 'Property_Estimator.pkl')
+    LinReg = joblib.load('Property_Estimator.pkl')
+    return(LinReg.fit(x,y))
 
-#Specifying Independent Variables and Dependent Variables
-mf_data = mfquant.ix[:,(1,2,3)].values
-mf_target = mfquant.ix[:,0].values
-mf_data_names = ['Cap','Sqft','bin_zip']
-x, y = scale(mf_data), mf_target
+def main():
+    df = pd.read_csv('C:/Users/sstandring/Dropbox/twerk werk/cre_values')
+    mfquant = transform(df)
+    #Specifying Independent Variables and Dependent Variables
+    mf_data = mfquant.ix[:,(1,2,3)].values
+    mf_target = mfquant.ix[:,0].values
+    mf_data_names = ['Cap','Sqft','bin_zip']
+    x, y = scale(mf_data), mf_target
+    LinReg = regression(x,y)
+    print (LinReg.score(x,y))
 
-#Regression Model 
-LinReg = LinearRegression(normalize=True)
-LinReg.fit(x,y)
-print (LinReg.score(x,y))
-#RFE(LinReg,3)
-#estimator = SVR(kernel='linear')
-#selector = RFE(estimator, 5, step=1)
-#selector.support_
-#selector.ranking_
-joblib.dump(LinReg, 'Property_Estimator.pkl')
-LinReg = joblib.load('Property_Estimator.pkl')
+if __name__ == '__main__':
+    main()
